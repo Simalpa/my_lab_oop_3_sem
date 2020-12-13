f010de232355eca12c84ff084ad2c789ee953d95
Simalpa <ferption@gmail.com>
2020-12-13 19:32:31 +0300

    add lab 6 7

---
+++ lab6_oop/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab6_oop
+{
+    class Program
+    {
+        class Equation
+        {
+            public virtual List<float> roots_equation(float a, float b, float c) { return new List<float>(); }
+        }
+
+        class LinearEquation : Equation
+        {
+            public override List<float> roots_equation(float a, float b, float c = 0)
+            {
+                return new List<float>{ b / a };
+            }
+        }
+
+        class QuadraticEquation : Equation
+        {
+            public override List<float> roots_equation(float a, float b, float c)
+            {
+                float D = b * b - 4 * a * c;
+
+                if (D > 0)
+                {
+                    float result1 = Convert.ToSingle((-b + Math.Sqrt(D)) / (2 * a));
+                    float result2 = Convert.ToSingle((-b - Math.Sqrt(D)) / (2 * a));
+                    return new List<float> {result1, result2};
+
+                }
+                else if (D == 0)
+                {
+                    float result = Convert.ToSingle(-b / (2 * a));
+                    return new List<float> { result };
+                }
+                else
+                    return new List<float>();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Equation line = new LinearEquation();
+            float a, b, c;
+            Console.Write("Введите значение a для линейного уравнения: ");
+            a = Convert.ToSingle(Console.ReadLine());
+            Console.Write("Введите значение b для линейного уравнения: ");
+            b = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine($"Решение линейного уравнения {a}x = {b}: ");
+            List<float> result = line.roots_equation(a, b, 0);
+
+            for (int i = 0; i < result.Count; i++)
+                Console.WriteLine($"Корень {i+1} равен {result[i]}");
+
+
+            Equation quadratic = new QuadraticEquation();
+            Console.Write("Введите значение a для квадратного уравнения: ");
+            a = Convert.ToSingle(Console.ReadLine());
+            Console.Write("Введите значение b для квадратного уравнения: ");
+            b = Convert.ToSingle(Console.ReadLine());
+            Console.Write("Введите значение с для квадратного уравнения: ");
+            c = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine($"Решение квадратного уравнения {a}x^2 + {b}x + {c} = 0 : ");
+            result = quadratic.roots_equation(a, b, c);
+            for (int i = 0; i < result.Count; i++)
+                Console.WriteLine($"Корень {i+1} равен {result[i]}");
+
+            if(result.Count == 0) { Console.WriteLine("Нет корней уравнения"); }
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ lab7oop/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab7oop
+{
+    class Program
+    {
+        public interface IOutput
+        {
+            void PrintFunction();
+        }
+
+        public interface ICalculate
+        {
+            float Calculate();
+        }
+
+        class LineFunction : IOutput, ICalculate
+        {
+            private float a, b, x;
+            public LineFunction(float _a, float _b, float _x)
+            {
+                a = _a;
+                b = _b;
+                x = _x;
+            }
+            public void PrintFunction()
+            {
+                Console.WriteLine("f(x) = ax + b");
+            }
+
+            public float Calculate()
+            {
+                return  a * x + b ;
+            }
+        }
+
+        class QuadrationFunction : IOutput, ICalculate
+        {
+            private float a, b, c, x;
+            public QuadrationFunction(float _a, float _b, float _c, float _x)
+            {
+                a = _a;
+                b = _b;
+                c = _c;
+                x = _x;
+            }
+
+            public void PrintFunction()
+            {
+                Console.WriteLine("f(x) = ax^2 + bx + c");
+            }
+
+            public float Calculate()
+            {
+                return  a * x * x + b * x + c;
+            }
+
+            
+        }
+
+        static void Main(string[] args)
+        {
+            float a, b, c, x;
+            Console.Write("Введите значение a для линейной функции: ");
+            a = Convert.ToSingle(Console.ReadLine());
+            Console.Write("Введите значение b для линейной функции: ");
+            b = Convert.ToSingle(Console.ReadLine());
+            Console.Write("Введите значение x для линейной функции: ");
+            x = Convert.ToSingle(Console.ReadLine());
+            LineFunction line = new LineFunction(a, b, x);
+            float f_x = line.Calculate();
+            Console.WriteLine($"f(x) = {a}*{x} + {b} = {f_x}");
+
+            Console.Write("Введите значение a для квадратичной функции: ");
+            a = Convert.ToSingle(Console.ReadLine());
+            Console.Write("Введите значение b для квадратичной функции: ");
+            b = Convert.ToSingle(Console.ReadLine());
+            Console.Write("Введите значение x для квадратичной функции: ");
+            c = Convert.ToSingle(Console.ReadLine());
+            Console.Write("Введите значение x для квадратичной функции: ");
+            x = Convert.ToSingle(Console.ReadLine());
+            QuadrationFunction quadration = new QuadrationFunction(a, b, c, x);
+            f_x = quadration.Calculate();
+            Console.WriteLine($"f(x) = {a}*{x}*{x} + {b}*{x} + {c} = {f_x}");
+
+
+            Console.ReadKey();
+        }
+    }
+}
