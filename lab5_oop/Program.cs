b576e52e2651336fd16185d84a9ce85340400646
Simalpa <ferption@gmail.com>
2020-12-17 10:54:47 +0300

    lab5_cinema

--- lab5_oop/Program.cs
+++ lab5_oop/Program.cs
@@ -2,10 +2,10 @@
 
 namespace Lab_5_oop
 {
-    public delegate void MyDelegat();
+    public delegate void MyDelegat(int countCom1, int countCom2, string nameCom1, string nameCom2);
     class FootballMatch
     {
-        public event MyDelegat onPrint;
+        public event MyDelegat printReview;
         public string NameCom1;
         public string NameCom2;
         public int CountCom1 = -1;
@@ -25,62 +25,96 @@ namespace Lab_5_oop
         public void PlayMatсh()
         {
             Random rand = new Random();
-            CountCom1 = rand.Next(11);
-            CountCom2 = rand.Next(11);
+            
+            CountCom1 = rand.Next(10);
+            do
+            {
+                CountCom2 = rand.Next(10);
+            }
+            while (CountCom2 == CountCom1);
+
+
+            if (CountCom1 > CountCom2)
+            {
+                Winner = NameCom1;
+            }
+            else
+            {
+                Winner = NameCom2;
+            }
 
             Console.WriteLine("Название первой команды: " + NameCom1);
             Console.WriteLine("Название второй команды: " + NameCom2);
             Console.WriteLine("Победила команда " + Winner + ", со счетом: " + CountCom1 + "-" + CountCom2);
-            Console.WriteLine();
             Console.WriteLine("Цена билета = " + CostTicket + " Рублей");
             Console.WriteLine("Дата проведения матча - " + Data);
         }
 
         public void PrintReviews()
         {
-
+            if (CountCom1 >= 0)
+            {
+                Console.WriteLine("Отзывы:");
+                printReview.Invoke(CountCom1, CountCom2, NameCom1, NameCom2);
+            }
+            else
+                Console.WriteLine("Результатов нет. Игра еще не прошла!");
         }
         
 
 
     }
 
-    class Message
+    class Viewer
     {
-        int waiting_invoice;
+        static Random rand = new Random();
+        string Name;
         int myComand;
-        public Message()
+        string[] plus_emotion = { "Вау!", "Офигенно!", "Неповторимо!", "Было, классно!", "Мне очень понравилось!", "Вот это да!"};
+        string[] minus_emotion = { "Фу!", "Что за игра?!", "Ну кто так играет!", "Какого х...!", "У них что ног нет?!", "Мда, разочарован!" };
+        public Viewer(string name)
         {
-            Random rand = new Random();
-            waiting_invoice = rand.Next(11);
+            Name = name;
+            myComand = rand.Next(1, 3);
         }
 
         public void PrintReview(int countCom1, int countCom2, string nameCom1, string nameCom2)
         {
+            Console.Write(Name + "(болеет за ");
+            if(myComand == 1)
+            {
+                Console.Write(nameCom1 + "): ");
+            }
+            else if(myComand == 2)
+            {
+                Console.Write(nameCom2 + "): ");
+            }
+
             if (myComand == 1 && countCom1 > countCom2)
             {
-
+                Console.Write(plus_emotion[rand.Next(plus_emotion.Length)] + " ");
             }
             else if (myComand == 1 && countCom1 < countCom2)
             {
-
+                Console.Write(minus_emotion[rand.Next(minus_emotion.Length)] + " ");
             }
             else if (myComand == 2 && countCom1 < countCom2)
             {
-
+                Console.Write(plus_emotion[rand.Next(plus_emotion.Length)] + " ");
             }
-            else if (myComand == 2 && countCom1 < countCom2)
+            else if (myComand == 2 && countCom1 > countCom2)
             {
-
+                Console.Write(minus_emotion[rand.Next(minus_emotion.Length)] + " ");
             }
 
-
+            Console.WriteLine();
 
         }
 
         public void ChangeMind()
         {
-
+            Random rand = new Random();
+            myComand = rand.Next(1, 3);
         }
     }
 
@@ -90,17 +124,19 @@ namespace Lab_5_oop
 
         static void Main(string[] args)
         {
-            EndFootballMatch mutch = new EndFootballMatch("Зенит", "Динамо", "3-2", "25.03.2015", 2500f, "Зенит");
-            EndFootballMatch mutch2 = new EndFootballMatch("Зенит", "Динамо", "3-2", "25.03.2015", 2500f, "Динамо");
-            Message mes = new Message();
-            //Handler handler1 = new Handler("Зенит", "Динамо", "3-2", "25.03.2015", 2500f, "Зенит");     
-            //MyDelegat myDelegat = new MyDelegat(handler1.PrintFields);
-            //myDelegat.Invoke();
-            mutch.onPrint += mes.onPrint;
-            mutch2.onPrint += mes.onPrint;
+            FootballMatch mutch = new FootballMatch("Зенит", "Динамо", "25.02.2021", 4000);
+            Viewer viewer1 = new Viewer("Виктор");
+            Viewer viewer2 = new Viewer("Данил");
+            Viewer viewer3 = new Viewer("Милана");
+            Viewer viewer4 = new Viewer("Дмитрий");
 
-            mutch.PrintFields();
-            mutch2.PrintFields();
+            mutch.printReview += viewer1.PrintReview;
+            mutch.printReview += viewer2.PrintReview;
+            mutch.printReview += viewer3.PrintReview;
+            mutch.printReview += viewer4.PrintReview;
+
+            mutch.PlayMatсh();
+            mutch.PrintReviews();
 
             Console.ReadKey();
         }
---
+++ lab5_oop_kurs3/Program.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab5_oop_kurs3
+{
+    class Program
+    {
+        public delegate void Delegate();
+        public delegate bool Delegate2(int seats);
+
+
+        class Cinema
+        {
+            public Delegate2 sell_tickets;
+            public Delegate showMovie;
+            public Delegate printReviews;
+            private string name;
+            private string adress;
+            private string name_premiere_film;
+            private int ticket_price;
+            private int number_of_seats;
+            private int bought_tickets = 0;
+
+            public Cinema(string _name, string _adress, string _name_premiere_film, int _ticket_price, int _number_of_seats)
+            {
+                name = _name;
+                adress = _adress;
+                name_premiere_film = _name_premiere_film;
+                ticket_price = _ticket_price;
+                number_of_seats = _number_of_seats;
+            }
+
+            public void SellTickets()
+            {
+                if (sell_tickets(number_of_seats - bought_tickets))
+                {
+                    bought_tickets++;
+                }
+                else
+                {
+                    Console.WriteLine("Распроданы все билеты!");
+                }
+            }
+
+            public void ShowMovie()
+            {
+                if (bought_tickets == 0)
+                {
+                    Console.WriteLine("Никто не купил билеты! Зачем показывать фильм?");
+                }
+                else
+                {
+                    showMovie();
+                    bought_tickets = 0;
+                }
+            }
+
+            public void PrintReviews()
+            {
+                Console.WriteLine("Кинотеатр: " + name);
+                Console.WriteLine("Адрес кинотеатра: " + adress);
+                Console.WriteLine("Фильм: " + name_premiere_film);
+                Console.WriteLine("Стоимость билетов: " + ticket_price);
+                Console.WriteLine("Куплено билетов: " + bought_tickets);
+                Console.WriteLine("Отзывы:");
+                printReviews();
+            }
+
+        }
+
+        class Viewer
+        {
+            Random rand = new Random();
+            private string name;
+            private int ratingFilm = int.MinValue;
+            bool watched = false;
+            bool bought_ticket = false;
+
+            public Viewer(string _name)
+            {
+                name = _name;
+            }
+
+            public void WatchFilm()
+            {
+                if (bought_ticket)
+                {
+                    ratingFilm = rand.Next(101);
+                    watched = false;
+                    bought_ticket = false;
+                }
+                else
+                {
+                    Console.WriteLine(name + ": Я еще даже не купил билет на фильм!");
+                }
+            }
+
+
+
+            public void ChangeMind()
+            {
+                if(watched)
+                {
+                    ratingFilm = rand.Next(101);
+                }
+                else
+                {
+                    Console.WriteLine(name + ": У меня пока что нет мнения об этом фильме. Я еще не смотрел фильм!");
+                }
+            }
+
+            public void SayReview()
+            {
+                if(watched)
+                {
+                    Console.WriteLine(name + ": ");
+                    if(0 <= ratingFilm && ratingFilm <= 10)
+                    {
+                        Console.WriteLine("Ужасно!");
+                    }
+                    else if (10 < ratingFilm && ratingFilm <= 20)
+                    {
+                        Console.WriteLine("Не стоит на это идти");
+                    }
+                    else if (20 < ratingFilm && ratingFilm <= 30)
+                    {
+                        Console.WriteLine("Ожидал большего");
+                    }
+                    else if (30 < ratingFilm && ratingFilm <= 40)
+                    {
+                        Console.WriteLine("Ну такое себе");
+                    }
+                    else if (40 < ratingFilm && ratingFilm <= 50)
+                    {
+                        Console.WriteLine("Сойдет");
+                    }
+                    else if (50 < ratingFilm && ratingFilm <= 60)
+                    {
+                        Console.WriteLine("Можно сходить один раз");
+                    }
+                    else if (70 < ratingFilm && ratingFilm <= 80)
+                    {
+                        Console.WriteLine("Достойное кинцо");
+                    }
+                    else if (80 < ratingFilm && ratingFilm <= 90)
+                    {
+                        Console.WriteLine("Однозначно рекомендую");
+                    }
+                    else if (90 < ratingFilm && ratingFilm <= 100)
+                    {
+                        Console.WriteLine("Это потрясающе, рекомендую всем");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine(name + ": Не могу ничего сказать, еще не смотрел фильм.");
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
